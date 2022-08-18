cc67720b67eca5d8cedcda830b08600b6fa490f2
Yana <bondarevayana@gmail.com>
2022-08-18 14:55:22 +0300

    Min and Mig added, Main demonstration added

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-int [] sales = {100,300,500,450};
+int [] sales = {100,300,500,450,250,690,850,320,10,235};
     SalesManager manager  = new SalesManager(sales);
     System.out.println ("Max" + manager.max());
+    System.out.println("Min" + manager.min());
+    System.out.println("Обрезанное среднее" + manager.mid());
     }
 }
--- src/SalesManager.java
+++ src/SalesManager.java
@@ -14,5 +14,24 @@ public class SalesManager {
         }
         return max;
     }
+
+    public int min() {
+        int min = sales [1];
+        for (int sale : sales) {
+            if (sale < min) {
+                min = sale;
+            }
+        }
+        return min;
+    }
+    public int mid() {
+    int summ = 0;
+
+    for (int i = 0; i < sales.length; i++)
+        summ += sales[i];
+    int mid = (summ - min() - max()) / (sales.length-2);
+    return mid;
+    }
+
 }
 
